04325ec21e021aeb1fc584ccb7bb6e199568da96
Dell <Dell@Dell-PC>
2017-05-17 15:39:34 +0900

    실습과제12

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ *<pre>
+ *org.dimigo.inheritance
+ *     |_SmartPhoneTest
+ *
+ * 1. 개요  : 
+ * 2. 작성일 : 2017. 5. 12.
+ *</pre>
+ *
+ * @author Dell
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+public static void main(String[] args) {
+	SmartPhone[] g ={	new IPhone("iphone7","애플",900000),
+	 new Galaxy("갤럭시 s8","삼성",800000)};
+	for(int i=0;i<2;i++){
+		System.out.println(g[i]);
+		g[i].turnOn();
+		g[i].pay();
+		g[i].useSpecialFunction();
+		g[i].turnOff();
+	}
+}
+}
